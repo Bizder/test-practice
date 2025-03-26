b2ff8a4c7e6296649be90cdbfbe383f7e8f8799c
Gergely Tokodi <togsaai@inf.elte.hu>
2025-03-26 02:17:41 +0100

    init

---
+++ Selenium/Test1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    [TestClass]
+    public class GoogleTests
+    {
+        [TestMethod]
+        public void OpenGoogleTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://google.com/ncr");
+            Assert.AreEqual("Google", driver.Title);
+
+            System.Threading.Thread.Sleep(3000);
+
+            try
+            {
+                var acceptButton = driver.FindElement(By.XPath("//div[contains(text(),'Accept all')]"));
+                acceptButton.Click();
+            }
+            catch (NoSuchElementException)
+            {
+                // Popup not shown – continue silently
+            }
+
+            System.Threading.Thread.Sleep(3000);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void GoogleSearchTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            var box = driver.FindElement(By.Name("q"));
+            box.SendKeys("Selenium WebDriver");
+            box.SendKeys(Keys.Enter);
+            Assert.IsTrue(driver.Title.Contains("Selenium WebDriver"));
+        }
+
+    }
+
+}
---
+++ Selenium/Test3.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+
+namespace SeleniumDemo.Tests
+{
+    [TestClass]
+    public class WaitTests
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void DuckDuckGoSearch_WithWait()
+        {
+            driver.Navigate().GoToUrl("https://duckduckgo.com");
+
+            var searchBox = driver.FindElement(By.Id("search_form_input_homepage"));
+            searchBox.SendKeys("selenium");
+            searchBox.SendKeys(Keys.Enter);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.TitleContains("selenium"));
+
+            Assert.IsTrue(driver.Title.ToLower().Contains("selenium"));
+        }
+    }
+}
